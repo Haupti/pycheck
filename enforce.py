4c860523a7e5bc7e11d1603107b994dad7edea80
marwin <marwin.ack@googlemail.com>
2024-04-20 00:44:50 +0200

    refactor and add some todos

--- enforce.py
+++ enforce.py
@@ -9,6 +9,13 @@ __CHECK_ERROR = -1
 class EnforceError(Exception):
     pass
 
+def __typerr_msg(actual_value, actual_type, expected_type):
+    return f"expected '{actual_value}' to be of type '{expected_type}' but actual is of type '{actual_type}'"
+def __typerr_lst_msg(actual_value, expected_type):
+    return f"expected all elements in '{actual_value}' to be of type '{expected_type}'"
+def __len_mismatch_msg(actual_value, expected_len, type_signature):
+    return  f"expected '{actual_value}' to have {expected_len} elements with type signature '{type_signature}'"
+
 def __show_types(types_t):
     if(len(types_t.__args__) == 1):
         return f"[{types_t.__args__[0].__name__}]"
@@ -16,26 +23,37 @@ def __show_types(types_t):
         return list(map(lambda x: x.__name__, types_t.__args__))
 
 def __is_one_of(types_t, arg) -> bool:
+    # checks if the argument matches any type in the given collection (type-union)
     return any(__assert_type(type_t, arg)[0] for type_t in types_t)
 
 def __check_tuple(type_t, arg):
+    # check if it is a tuple
     if type(arg).__name__ != "tuple":
-        return (__CHECK_FAILURE, f"expected '{arg}' to be of type 'tuple' but actual is of type '{type(arg).__name__}'")
+        return (__CHECK_FAILURE, __typerr_msg(arg, type(arg).__name__,'tuple'))
+
+    # check if lenght matches
     if(len(type_t.__args__) != len(arg)):
-        return (__CHECK_FAILURE, f"expected '{arg}' to have {len(type_t.__args__)} elements with type signature '{type(arg).__args__}'")
+        return (__CHECK_FAILURE, __len_mismatch_msg(arg, len(type_t.__args__), type(arg).__args__))
+
+    # check if all entries are of the correct type
     for index, (t, v) in enumerate(zip(type_t.__args__, arg)):
         result, msg = __assert_type(t, v)
         if(result != __CHECK_SUCCESS):
             return (result, f"in tuple position {index}: {msg}")
+
+    # no return so far -> everything ok
     return (__CHECK_SUCCESS, "")
 
 def __check_list(type_t, arg):
+    # check if it is a list
     if type(arg).__name__ != "list":
-        return (__CHECK_FAILURE, f"expected '{arg}' to be of type 'list' but actual is of type '{type(arg).__name__}'")
-    return (all(__is_one_of(type_t.__args__, arg_elem) for arg_elem in arg), f"expected all elements in '{arg}' to be of type 'union{__show_types(type_t)}'")
+        return (__CHECK_FAILURE, __typerr_msg(arg,type(arg).__name__, "list"))
+
+    # check if all values in the list are of the expected type(s)
+    return (all(__is_one_of(type_t.__args__, arg_elem) for arg_elem in arg), __typerr_lst_msg(arg, f'union{__show_types(type_t)}'))
 
 def __check_primitive(type_t, arg):
-    return (isinstance(arg, type_t), f"expected '{arg}' to be of type '{type_t.__name__}' but is of type '{type(arg).__name__}'")
+    return (isinstance(arg, type_t), __typerr_msg(arg, type(arg).__name__, type_t.__name__))
 
 def __assert_type(type_t, arg) -> (bool, str):
     match type_t.__name__:
@@ -45,14 +63,19 @@ def __assert_type(type_t, arg) -> (bool, str):
             return __check_list(type_t, arg)
         case "int" | "str" | "float" | "bool":
             return __check_primitive(type_t, arg)
+        # TODO cases: union, class
         case _:
             return (__CHECK_ERROR, f"type '{type_t.__name__}' cannot be enfoced")
+
+#TODO a utility function that can be called in code if needed
+
 
 def __enforce(fn, args):
     names = fn.__code__.co_varnames
     types = get_type_hints(fn)
     type_keys = types.keys()
 
+    # check all the types of function arguments, if they have annotation
     for index, name in enumerate(names):
         result, msg = __assert_type(types[name], args[index])
         if(result == __CHECK_FAILURE):
@@ -60,10 +83,12 @@ def __enforce(fn, args):
         elif(result == __CHECK_ERROR):
                 raise EnforceError(msg)
 
+    # return the expected type of the return-value or 'nothing', if it should not be checked
     try:
         return types['return']
     except KeyError:
         return __NO_RETURN_TYPE
+
 
 def __return(type_t, val):
     if(type(val) == type_t):
