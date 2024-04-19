5069c9ca7d08d10cbf8ccdb4e11966a902027e9d
marwin <marwin.ack@googlemail.com>
2024-04-19 22:19:22 +0200

    nested lists work

--- enforce.py
+++ enforce.py
@@ -2,15 +2,29 @@ from typing import *
 
 __NO_RETURN_TYPE = 0
 
+def __is_one_of(types_t, arg) -> bool:
+    return any(__assert_type(type_t, arg)[0] for type_t in types_t)
+
+def __assert_type(type_t, arg) -> (bool, str):
+    match type_t.__name__:
+        case "list":
+            if type(arg).__name__ != "list":
+                return (False, f"expected '{arg}' to be of type 'list' but actual is of type '{type(arg).__name__}'")
+            return (all(__is_one_of(type_t.__args__, arg_elem) for arg_elem in arg), f"expected all elements in '{arg}' to be of type 'oneof{type_t.__args__}'")
+        case "int" | "str" | "float" | "bool":
+            return (isinstance(arg, type_t), f"expected '{arg}' to be of type '{type_t.__name__}' but is of type '{type(arg).__name__}'")
+        case _ :
+            return (False, "type not checked")
+
 def __enforce(fn, args):
     names = fn.__code__.co_varnames
     types = get_type_hints(fn)
     type_keys = types.keys()
 
     for index, name in enumerate(names):
-        if(name in type_keys):
-            if not type(args[index]) == types[name]:
-                raise TypeError(f"expected type of '{name}' is {types[name]}, but actual was {type(args[index])}")
+        is_success, msg = __assert_type(types[name], args[index])
+        if not is_success:
+            raise TypeError(msg)
     try:
         return types['return']
     except KeyError:
@@ -32,7 +46,7 @@ def enforce(fn):
     return wrapper
 
 @enforce
-def my_func(a: int, b:int) -> int:
-    return b
+def my_func(a: list[int, list[float]], b: int) -> int:
+    return a[0]
 
-my_func(1,2)
+my_func([1, [1.5]], 1)
