6b8f0deb2b738aa81604409e8badf6c7850f0ed0
marwin <marwin.ack@googlemail.com>
2024-04-20 00:01:08 +0200

    check for tuples

--- enforce.py
+++ enforce.py
@@ -18,15 +18,34 @@ def __show_types(types_t):
 def __is_one_of(types_t, arg) -> bool:
     return any(__assert_type(type_t, arg)[0] for type_t in types_t)
 
+def __check_tuple(type_t, arg):
+    if type(arg).__name__ != "tuple":
+        return (__CHECK_FAILURE, f"expected '{arg}' to be of type 'tuple' but actual is of type '{type(arg).__name__}'")
+    if(len(type_t.__args__) != len(arg)):
+        return (__CHECK_FAILURE, f"expected '{arg}' to have {len(type_t.__args__)} elements with type signature '{type(arg).__args__}'")
+    for index, (t, v) in enumerate(zip(type_t.__args__, arg)):
+        result, msg = __assert_type(t, v)
+        if(result != __CHECK_SUCCESS):
+            return (result, f"in tuple position {index}: {msg}")
+    return (__CHECK_SUCCESS, "")
+
+def __check_list(type_t, arg):
+    if type(arg).__name__ != "list":
+        return (__CHECK_FAILURE, f"expected '{arg}' to be of type 'list' but actual is of type '{type(arg).__name__}'")
+    return (all(__is_one_of(type_t.__args__, arg_elem) for arg_elem in arg), f"expected all elements in '{arg}' to be of type 'union{__show_types(type_t)}'")
+
+def __check_primitive(type_t, arg):
+    return (isinstance(arg, type_t), f"expected '{arg}' to be of type '{type_t.__name__}' but is of type '{type(arg).__name__}'")
+
 def __assert_type(type_t, arg) -> (bool, str):
     match type_t.__name__:
+        case "tuple":
+            return __check_tuple(type_t, arg)
         case "list":
-            if type(arg).__name__ != "list":
-                return (__CHECK_FAILURE, f"expected '{arg}' to be of type 'list' but actual is of type '{type(arg).__name__}'")
-            return (all(__is_one_of(type_t.__args__, arg_elem) for arg_elem in arg), f"expected all elements in '{arg}' to be of type 'oneof{__show_types(type_t)}'")
+            return __check_list(type_t, arg)
         case "int" | "str" | "float" | "bool":
-            return (isinstance(arg, type_t), f"expected '{arg}' to be of type '{type_t.__name__}' but is of type '{type(arg).__name__}'")
-        case _ :
+            return __check_primitive(type_t, arg)
+        case _:
             return (__CHECK_ERROR, f"type '{type_t.__name__}' cannot be enfoced")
 
 def __enforce(fn, args):
@@ -36,9 +55,9 @@ def __enforce(fn, args):
 
     for index, name in enumerate(names):
         result, msg = __assert_type(types[name], args[index])
-        if(__CHECK_FAILURE):
+        if(result == __CHECK_FAILURE):
             raise TypeError(f"'{name}' has wrong type: {msg}")
-        elif(__CHECK_ERROR):
+        elif(result == __CHECK_ERROR):
                 raise EnforceError(msg)
 
     try:
@@ -65,4 +84,4 @@ def enforce(fn):
 def my_func(a: tuple[int, float], b: int) -> int:
     return a[0]
 
-my_func([1, "b"], 1)
+my_func((1, 1.0), 1)
